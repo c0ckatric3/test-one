8d729593932368f0e2609b84b6a59c1804ce1007
Tea Lee <volotova20001@gmail.com>
2020-07-17 12:22:52 +0300

    Adaptive fix

--- src/components/team-block/team-block.js
+++ src/components/team-block/team-block.js
@@ -8,7 +8,7 @@ const TeamBlock = () => {
             <span className="title black">MEET OUR BEAUTIFUL TEAM</span>
             <hr />
             <span className="info black">
-            We are a small team of designers and developers, who help brands with big ideas.  
+            We are a small team of designers and developers, who help brands with big ideas.
             </span>
             <TeamList />
         </div>
